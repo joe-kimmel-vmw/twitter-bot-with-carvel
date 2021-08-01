a84c443a045d144673ff9b70773114be3b6e80f8
Joe Kimmel <jkimmel@vmware.com>
2021-07-31 19:11:51 -0700

    tweets now generated using advanced AI techniques and deepfakes

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import os
+import random
 
 import tweepy
+
+def tweet():
+  return random.choice([
+    "Help I'm a bot",
+    "tweet. tweet. twiddly tweet",
+    "Pleased to tweet you",
+    "Do twitbots dream of electic birds?"
+  ])
+
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(os.getenv("TWIT_CONSUMER_KEY"), os.getenv("TWIT_CONSUMER_SECRET"))
@@ -10,5 +20,5 @@ auth.set_access_token(os.getenv("TWIT_ACCESS_TOKEN"), os.getenv("TWIT_ACCESS_TOK
 api = tweepy.API(auth)
 
 # Create a tweet
-api.update_status("Help I'm a bot")
+api.update_status(tweet())
 
