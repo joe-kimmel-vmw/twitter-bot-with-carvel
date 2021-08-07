c59dee3c21c6c4d9ae220198b4d873a04528b4ef
Joe Kimmel <jkimmel@vmware.com>
2021-08-06 20:40:00 -0700

    disentangling the package repo from the app repo but also i got docker hub rate limited so i'll try again tomorrow.

--- build.sh
+++ build.sh
@@ -2,40 +2,44 @@
 
 set -eux
 
-VERSION=0.1.0
-REPOTAG=jkimmelvmware/toy-projects:artificialtweetener-$VERSION
+IMGVERSION=0.1.0
+IMGREPOPATH="jkimmelvmware/toy-projects:artificialtweetener"
+IMGREPOTAG=${IMGREPOPATH}-${IMGVERSION}
+PKGVERSION=1.0.0
+PKGREPOPATH="jkimmelvmware/my-pkg-repo:packages"
+PKGREPOTAG=${PKGREPOPATH}-${PKGVERSION}
 REPOHOST="index.docker.io"
 APPNAME="artificial-tweetener.github.com"
 
-docker build . -t ${REPOTAG}
+docker build . -t ${IMGREPOTAG}
 # may need to `docker login --username="jkimmelvmware"` if access is denied
-docker push ${REPOTAG}
+docker push ${IMGREPOTAG}
 
 cat > distribute/carvel/package-contents/config/values.yml <<- EOF
 #@data/values
 ---
-docker_image: ${REPOTAG}
+docker_image: ${IMGREPOTAG}
 EOF
 
 kbld -f distribute/carvel/package-contents/config/ --imgpkg-lock-output distribute/carvel/package-contents/.imgpkg/images.yml
 
-imgpkg push -b ${REPOHOST}/${REPOTAG} -f distribute/carvel/package-contents/
+imgpkg push -b ${REPOHOST}/${IMGREPOTAG} -f distribute/carvel/package-contents/
 
 # TODO (should i do this with ytt instead?)
-cat > distribute/carvel/my-pkg-repo/packages/artificial-tweetener.github.com/${VERSION}.yml <<- EOF
+cat > distribute/carvel/my-pkg-repo/packages/artificial-tweetener.github.com/${IMGVERSION}.yml <<- EOF
 apiVersion: data.packaging.carvel.dev/v1alpha1
 kind: Package
 metadata:
-  name: ${APPNAME}.${VERSION}
+  name: ${APPNAME}.${IMGVERSION}
 spec:
   refName: ${APPNAME}
-  version: ${VERSION}
+  version: ${IMGVERSION}
   releaseNotes: try not to be so jealous
   template:
     spec:
       fetch:
       - imgpkgBundle:
-          image: ${REPOHOST}/${REPOTAG}
+          image: ${REPOHOST}/${IMGREPOTAG}
       template:
       - ytt:
           paths:
@@ -50,7 +54,7 @@ EOF
 
 kbld -f distribute/carvel/my-pkg-repo/packages/ --imgpkg-lock-output distribute/carvel/my-pkg-repo/.imgpkg/images.yml
 
-imgpkg push -b ${REPOHOST}/${REPOTAG} -f distribute/carvel/my-pkg-repo
+imgpkg push -b ${REPOHOST}/${PKGREPOTAG} -f distribute/carvel/my-pkg-repo
 
 cat > distribute/carvel/consumer-repo.yml <<- EOF
 ---
@@ -61,7 +65,7 @@ metadata:
 spec:
   fetch:
     imgpkgBundle:
-      image: ${REPOHOST}/packages/my-pkg-repo/${VERSION}
+      image: ${REPOHOST}/${IMGREPOTAG}
 EOF
 
 cat > distribute/carvel/pkginstall.yml <<-EOF
@@ -75,7 +79,7 @@ spec:
   packageRef:
     refName: ${APPNAME}
     versionSelection:
-      constraints: ${VERSION}
+      constraints: ${IMGVERSION}
   values:
   - secretRef:
       name: opensesame
@@ -92,5 +96,14 @@ stringData:
 EOF
 
 # kapp deploy -a default-ns-rbac -f https://raw.githubusercontent.com/vmware-tanzu/carvel-kapp-controller/develop/examples/rbac/default-ns.yml -y
+
+kapp deploy -a repo -f distribute/carvel/consumer-repo.yml -y
+echo "allowing 10 seconds for packagerepository to reconcile..."
+sleep 3
+kubectl get packagerepository
+sleep 4
+kubectl get packagerepository
+sleep 3
+kubectl get packagerepository
 
 kapp deploy -a ${APPNAME} -f distribute/carvel/pkginstall.yml -y
